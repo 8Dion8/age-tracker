64864982c7629dca1f853779a9c998ac6038c2f4
8Dion8 <shvartserinfo@gmail.com>
2022-01-31 14:30:41 +0000

    text centering

--- clock.py
+++ clock.py
@@ -9,6 +9,8 @@ import sys
 #seconds_passed = time_passed.seconds
 
 
+
+
 def update_years():
     birth_date = datetime.datetime(2005, 12, 15)#, 12, 15)
     date_now = datetime.datetime.now()
@@ -24,9 +26,20 @@ def update_years():
     milliseconds_passed = str(int(seconds_passed%60*1000%1000)).zfill(3)
     seconds_passed = int(seconds_passed%60)
     
+    terminal_width  = os.get_terminal_size().columns
+    terminal_height = os.get_terminal_size().lines
+    lines_printed = 7
+    vertical_centering = (terminal_height - lines_printed) // 2
 
     os.system("clear")
-    print(f"{years_passed} years\n{days_passed} days\n{hours_passed} hours\n{minutes_passed} minutes\n{seconds_passed} seconds\n{milliseconds_passed} milliseconds \n...have passed in your life.")
+    print("\n"*vertical_centering)
+    print(f"{years_passed} years".center(terminal_width))
+    print(f"{days_passed} days".center(terminal_width))
+    print(f"{hours_passed} hours".center(terminal_width))
+    print(f"{minutes_passed} minutes".center(terminal_width))
+    print(f"{seconds_passed} seconds".center(terminal_width))
+    print(f"{milliseconds_passed} milliseconds".center(terminal_width))
+    print("...have passed in your life.".center(terminal_width))
 
 if __name__ == "__main__":
     while True:
